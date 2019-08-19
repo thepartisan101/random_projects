9c62183302e9d75afef6eeeede510e5498c9febf
thepartisan101 <9511307+thepartisan101@users.noreply.github.com>
2019-08-19 14:50:20 +0300

    Update api methods

--- flask_api/app.py
+++ flask_api/app.py
@@ -31,7 +31,43 @@ class User(Resource):
             return "User not found", 404
 
     def post(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("age")
+        parser.add_argument("occupation")
+        args = parser.parser_args()
+
+        for user in users:
+            if(name == user["name"]):
+                return "User with name {} already exists".format(name), 400
+
+        user = {
+            "name": name,
+            "age": args["age"],
+            "occupation": args["occupation"]
+        }
+        users.append(user)
+        return user, 201
 
     def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("age")
+        parser.add_argument("occupation")
+        args = parser.parse_args()
+
+        for user in users:
+            if(name == user["name"]):
+                user["age"] = args["age"]
+                user["occupation"] = args["occupation"]
+                return user, 200
+
+        user = {
+            "name": name,
+            "age": args["age"],
+            "occupation": args["occupation"]
+        }
+        users.append(user)
+        return user, 201
 
     def delete(self, name):
+        global usersusers = [user for user in users if user["name"] != name]
+        return "{} is deleted".format(name), 200
