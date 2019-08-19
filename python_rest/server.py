3cfdf399b78d45d2579018d5651347b30f63cfaf
thepartisan101 <9511307+thepartisan101@users.noreply.github.com>
2019-08-19 11:56:35 +0300

    Create class to connect to db and resources

--- python_rest/server.py
+++ python_rest/server.py
@@ -2,4 +2,21 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
-from flask.ext.jsonpify import jsonpify
+from flask.ext.jsonpify import jsonify
+
+db_connect = create_engine('sqlite:///chinnok.db')
+app = Flask(__name__)
+api = Api(app)
+
+class Employees(Resource):
+    def get(self):
+        conn = db_connect.connect() #Connect to database
+        query = conn.execute("select * from employees") # Perfroms a query and return json results
+        return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches 1st collumn, the Employee ID
+
+class Tracks(Resource):
+    def get(self):
+        conn = db_connnect.connect()
+        query = conn.execute("select trackid, name composer, unitprice from tracks;")
+        result = {'data': [dict(zip(tuple (query.keys()), i)) for i in query.cursor]}
+        return jsonify(result)
