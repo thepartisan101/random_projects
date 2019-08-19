0b7770049659cfc52f370fa589eadd19103c0ac5
thepartisan101 <9511307+thepartisan101@users.noreply.github.com>
2019-08-19 16:52:55 +0300

    Create classes and methods for schema

--- graphene/schema.py
+++ graphene/schema.py
@@ -8,3 +8,20 @@ def extract(url):
     extracted = extraction.Extractor().extract(html, source_url= url)
     return extracted
 
+class Website(graphene.ObjectType):
+    url = graphene.String(required=True)
+    title = graphene.string()
+    description = graphene.String()
+    image = graphene.String()
+
+class Query(graphene.ObjectType):
+    website = graphene.Field(Website, url=graphene.String())
+
+    def resolve_website(self, info, url):
+        extracted = extract(url)
+        return Website(url=url,
+                        title=extracted.title,
+                        description=extracted.description,
+                        image=extracted.image)
+
+schema = graphene.Schema(query=Query)
