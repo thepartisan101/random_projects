9f64b88f944739975729fec65ff27e09194329a7
thepartisan101 <9511307+thepartisan101@users.noreply.github.com>
2019-08-19 12:04:42 +0300

    Add api resource classes, endpoints

--- python_rest/server.py
+++ python_rest/server.py
@@ -20,3 +20,17 @@ class Tracks(Resource):
         query = conn.execute("select trackid, name composer, unitprice from tracks;")
         result = {'data': [dict(zip(tuple (query.keys()), i)) for i in query.cursor]}
         return jsonify(result)
+
+class Employees_Name(Resource):
+    def get(self, employee_id):
+        conn = db_connect.connect()
+        query = conn.execute('select * from employees where EmployeeId = %d' %int(employee_id))
+        result = {'data':[dict(zip(tuple (query.keys()), i)) for i in query.cursor]}
+        return jsonify(result)
+
+api.add_resource(Employees, '/employees') # Route_1
+api.add_resource(Tracks, '/tracks') # Route_2
+api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
+
+if __name__ == "__main__":
+    app.run(port='5002')
